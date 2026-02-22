58b3659b5c746cccadcd70028dda204db1c62de2
Fernando Martinez <TU-CORREO-QUE-USAS-EN-GITHUB@gmail.com>
2026-02-22 00:43:26 -0600

    primer commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1 className="CENTRO">Hola Fernando 👋</h1>
+      <input placeholder="USUARIO"className="usuario"/>
+      <input placeholder="CONTRASEÑA" className="contraseña"/>
+      <p>Mi primera página con React</p>
+    </div>
+  )
+}
+
+export default App
