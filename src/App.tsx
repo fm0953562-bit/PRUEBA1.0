6bd425ced4ff5906c088b78593fd9d75e2cfe293
Fernando Martinez <TU-CORREO-QUE-USAS-EN-GITHUB@gmail.com>
2026-02-22 00:55:43 -0600

    Nueva Actualizacion

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
       <h1 className="CENTRO">Hola Fernando 👋</h1>
       <input placeholder="USUARIO"className="usuario"/>
       <input placeholder="CONTRASEÑA" className="contraseña"/>
+      <h2>POR FIN LA PUDE PUBLICAR</h2>
       <p>Mi primera página con React</p>
     </div>
   )
